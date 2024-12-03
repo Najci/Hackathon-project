80d40750b9c748774525ad24c0f7c802bcd0b7dc
Naj <hidanovicmax9@gmail.com>
2024-12-03 08:45:38 +0100

    commit -m "Done"

--- client/vite.config.js
+++ client/vite.config.js
@@ -5,4 +5,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   base: '/login',
+  build: {
+    outDir: 'dist', // Default output directory
+  },
 })
