c56e0294abf596110b2fa5f12210823824758248
Naj <hidanovicmax9@gmail.com>
2024-12-08 01:34:07 +0100

    changes reverted for hackathon

--- Server/server.js
+++ Server/server.js
@@ -34,7 +34,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(cors({
-    origin: "https://hackathon-project-sbuj.onrender.com",
+    origin: "http://localhost:5173",
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     credentials: true
   }));
--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -22,7 +22,7 @@ const Login = ({ CreateCookie, user }) => {
     const form = new FormData(e.target); 
     const formData = Object.fromEntries(form.entries())
 
-     axios.post('https://hackathon-project-sbuj.onrender.com/login', formData)
+     axios.post('http://localhost:3000/login', formData)
     .then(function (response) {
       CreateCookie(response.data.user)
       navigate('/dashboard')
